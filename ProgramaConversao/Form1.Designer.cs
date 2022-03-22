a23b752ebaa07ed9b054ea8771b62b3c7fcbc54a
Fernando Santana <fernando.santana10000@gmail.com>
2022-03-21 22:28:37 -0300

    commit

---
+++ ProgramaConversao/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace TrabalhoConversao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonConverter = new System.Windows.Forms.Button();
+            this.comboBoxBaseValor = new System.Windows.Forms.ComboBox();
+            this.comboBoxBaseConvert = new System.Windows.Forms.ComboBox();
+            this.textBoxValorConvert = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBoxResultado = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonConverter);
+            this.panel1.Controls.Add(this.comboBoxBaseValor);
+            this.panel1.Controls.Add(this.comboBoxBaseConvert);
+            this.panel1.Controls.Add(this.textBoxValorConvert);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 138);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonConverter
+            // 
+            this.buttonConverter.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConverter.Location = new System.Drawing.Point(393, 65);
+            this.buttonConverter.Name = "buttonConverter";
+            this.buttonConverter.Size = new System.Drawing.Size(149, 29);
+            this.buttonConverter.TabIndex = 6;
+            this.buttonConverter.Text = "Converter";
+            this.buttonConverter.UseVisualStyleBackColor = true;
+            this.buttonConverter.Click += new System.EventHandler(this.buttonConverter_Click);
+            // 
+            // comboBoxBaseValor
+            // 
+            this.comboBoxBaseValor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxBaseValor.FormattingEnabled = true;
+            this.comboBoxBaseValor.Items.AddRange(new object[] {
+            "Binário",
+            "Octal",
+            "Decimal",
+            "Hexadecimal"});
+            this.comboBoxBaseValor.Location = new System.Drawing.Point(624, 23);
+            this.comboBoxBaseValor.Name = "comboBoxBaseValor";
+            this.comboBoxBaseValor.Size = new System.Drawing.Size(153, 21);
+            this.comboBoxBaseValor.TabIndex = 5;
+            // 
+            // comboBoxBaseConvert
+            // 
+            this.comboBoxBaseConvert.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxBaseConvert.FormattingEnabled = true;
+            this.comboBoxBaseConvert.Items.AddRange(new object[] {
+            "Binário",
+            "Octal",
+            "Decimal",
+            "Hexadecimal"});
+            this.comboBoxBaseConvert.Location = new System.Drawing.Point(177, 69);
+            this.comboBoxBaseConvert.Name = "comboBoxBaseConvert";
+            this.comboBoxBaseConvert.Size = new System.Drawing.Size(153, 21);
+            this.comboBoxBaseConvert.TabIndex = 4;
+            // 
+            // textBoxValorConvert
+            // 
+            this.textBoxValorConvert.Location = new System.Drawing.Point(146, 23);
+            this.textBoxValorConvert.Name = "textBoxValorConvert";
+            this.textBoxValorConvert.Size = new System.Drawing.Size(263, 20);
+            this.textBoxValorConvert.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(447, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Informe a base do valor:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Base para converter:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor a converter:";
+            // 
+            // richTextBoxResultado
+            // 
+            this.richTextBoxResultado.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxResultado.Location = new System.Drawing.Point(0, 138);
+            this.richTextBoxResultado.Name = "richTextBoxResultado";
+            this.richTextBoxResultado.Size = new System.Drawing.Size(800, 312);
+            this.richTextBoxResultado.TabIndex = 1;
+            this.richTextBoxResultado.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBoxResultado);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversão entre bases númericas";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBoxResultado;
+        private System.Windows.Forms.TextBox textBoxValorConvert;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxBaseConvert;
+        private System.Windows.Forms.Button buttonConverter;
+        private System.Windows.Forms.ComboBox comboBoxBaseValor;
+    }
+}
+
---
+++ ProgramaConversao/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabalhoConversao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConverter_Click(object sender, EventArgs e)
+        {
+            string valor1 = textBoxValorConvert.Text.ToUpper();
+            short base1 = 0, base2 = 0;
+            string caracteres = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";      
+            if (comboBoxBaseValor.SelectedIndex == 0)
+            {
+                base1 = 2;
+            }
+            else if (comboBoxBaseValor.SelectedIndex == 1)
+            {
+                base1 = 8;
+            }
+            else if (comboBoxBaseValor.SelectedIndex == 2)
+            {
+                base1 = 10;
+            }
+            else if (comboBoxBaseValor.SelectedIndex == 3)
+            {
+                base1 = 16;
+            }
+            if (comboBoxBaseConvert.SelectedIndex == 0)
+            {
+                base2 = 2;
+            }
+            else if (comboBoxBaseConvert.SelectedIndex == 1)
+            {
+                base2 = 8;
+            }
+            else if (comboBoxBaseConvert.SelectedIndex == 2)
+            {
+                base2 = 10;
+            }
+            else if (comboBoxBaseConvert.SelectedIndex == 3)
+            {
+                base2 = 16;
+            }
+            if (!(valor1 != "" && base1 != 0 && base2 != 0))
+            {
+                MessageBox.Show("Selecione alguma base ou preencha um número valido para ser convertido!", "Erro");
+                return;
+            }
+            for (int i = 0; i < valor1.Trim().Length; i++)
+            {
+
+                for (int x = base1; x < caracteres.Length; x++)
+                {
+
+                    if (!valor1.Contains(caracteres[x]))
+                    {
+                        
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("Valor fora da faixa de inteiro 64 bits ou contém dígitos inválidos", "Erro");
+                        return;
+                    }
+                }
+
+            }
+            long resul = baseToDecimal(valor1, base1);
+            if (base2 == 10)
+            {
+                richTextBoxResultado.AppendText(valor1 + " Convertido para " + comboBoxBaseConvert.SelectedItem.ToString() + " " + resul.ToString() + Environment.NewLine);
+            }
+            else
+            {
+                richTextBoxResultado.AppendText(valor1 + " Convertido para " + comboBoxBaseConvert.SelectedItem.ToString() + " " + decimalToBase(resul, base2) + Environment.NewLine);
+            }
+
+
+        }
+        private string decimalToBase(long n, short b)
+        {
+            string resultado = null;
+            while (n / b >= 1)
+            {
+                if (b == 16)
+                {
+                    if ((n % b) == 10)
+                    {
+                        resultado = "A" + resultado;
+                    }
+                    else if ((n % b) == 11)
+                    {
+                        resultado = "B" + resultado;
+                    }
+                    else if ((n % b) == 12)
+                    {
+                        resultado = "C" + resultado;
+                    }
+                    else if ((n % b) == 13)
+                    {
+                        resultado = "D" + resultado;
+                    }
+                    else if ((n % b) == 14)
+                    {
+                        resultado = "E" + resultado;
+                    }
+                    else if ((n % b) == 15)
+                    {
+                        resultado = "F" + resultado;
+                    }
+                    else
+                    {
+                        resultado = Convert.ToString(n % b) + resultado;
+                    }
+                }
+                else
+                {
+                    resultado = Convert.ToString(n % b) + resultado;
+                }
+
+                n = n / b;
+            }
+            resultado = Convert.ToString(n) + resultado;
+
+            return resultado;
+        }
+        private long baseToDecimal(string n, short b)
+
+        {
+            long resultado = 0;
+            long num = 0;
+            for (int i = 1; i <= n.Length; i++)
+            {
+                string ultimo = n[n.Length - i].ToString();
+                if (ultimo == "A")
+                {
+                    num = 10;
+                }
+                else if (ultimo == "B")
+                {
+                    num = 11;
+                }
+                else if (ultimo == "C")
+                {
+                    num = 12;
+                }
+                else if (ultimo == "D")
+                {
+                    num = 13;
+                }
+                else if (ultimo == "E")
+                {
+                    num = 14;
+                }
+                else if (ultimo == "F")
+                {
+                    num = 15;
+                }
+                else
+                {
+                    num = Convert.ToInt64(ultimo);
+                }
+
+                resultado += num * (int)Math.Pow(b, i - 1);
+            }
+
+            return resultado;
+        }
+
+    }
+}
